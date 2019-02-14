77ef7e29f46806aa1a629bf52d84d0d0da4ff845
Bart Zaino <bzaino@asa.org>
2019-02-14 10:05:42 -0500

    Push files

---
+++ DI.MVC/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DI.MVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterType<CustomerRepository>().As<ICustomerRepository>();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(t => t.Name.EndsWith("Controller"));
+
+            IContainer container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
